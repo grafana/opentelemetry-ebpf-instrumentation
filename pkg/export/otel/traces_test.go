da6ed7d33287e7cd2baf3b55980f732cd17126b7
Marc Tuduri <marctc@protonmail.com>
2025-05-28 11:15:38 +0200

    fix rejections

--- pkg/export/otel/traces_test.go
+++ pkg/export/otel/traces_test.go
@@ -349,6 +349,7 @@ func TestGenerateTraces(t *testing.T) {
 			ParentSpanID: parentSpanID,
 			TraceID:      traceID,
 			SpanID:       spanID,
+			Service:      svc.Attrs{UID: svc.UID{Name: "1"}},
 		}
 		traces := GenerateTraces(span, "host-id", map[attr.Name]struct{}{}, []attribute.KeyValue{})
 
@@ -631,8 +632,8 @@ func TestTraceSampling(t *testing.T) {
 			Method:       "GET",
 			Route:        "/test" + strconv.Itoa(i),
 			Status:       200,
-			Service:      svc.Attrs{},
 			TraceID:      RandomTraceID(),
+			Service:      svc.Attrs{UID: svc.UID{Name: strconv.Itoa(i)}},
 		}
 		spans = append(spans, span)
 	}
@@ -703,7 +704,7 @@ func TestTraceSkipSpanMetrics(t *testing.T) {
 			Method:       "GET",
 			Route:        "/test" + strconv.Itoa(i),
 			Status:       200,
-			Service:      svc.Attrs{},
+			Service:      svc.Attrs{UID: svc.UID{Name: strconv.Itoa(i)}},
 			TraceID:      RandomTraceID(),
 		}
 		spans = append(spans, span)
--- pkg/internal/traces/read_decorator.go
+++ pkg/internal/traces/read_decorator.go
@@ -3,7 +3,6 @@ package traces
 import (
 	"context"
 	"log/slog"
-	"strconv"
 
 	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/app/request"
 	"github.com/open-telemetry/opentelemetry-ebpf-instrumentation/pkg/internal/exec"
@@ -84,8 +83,11 @@ func hostNamePIDDecorator(cfg *InstanceIDConfig) decorator {
 	}
 
 	// caching instance ID composition for speed and saving memory generation
-	return func(s *svc.Attrs, hostPID int) {
-		s.UID.Instance = fullHostName + ":" + strconv.Itoa(hostPID)
+	return func(s *svc.Attrs, _ int) {
+		// FIXME - the pid here is causing a cardinality explosion of the
+		// 'target_info' metrics when instrumenting processes that fork()
+		// s.UID.Instance = fullHostName + ":" + strconv.Itoa(hostPID)
+		s.UID.Instance = fullHostName
 		s.HostName = fullHostName
 	}
 }
