fa9ac74c4bf9a29bed88da59061566fe8846650a
Marc Tuduri <marctc@protonmail.com>
2025-05-28 14:46:12 +0200

    fix stuff

--- pkg/instrumenter/beyla_test_linux.go
+++ pkg/instrumenter/beyla_test_linux.go
@@ -1,6 +1,6 @@
 //go:build linux
 
-package components
+package instrumenter
 
 import (
 	"bytes"
@@ -65,7 +65,7 @@ func TestRunDontPanic(t *testing.T) {
 			require.NoError(t, cfg.Validate())
 
 			require.NotPanics(t, func() {
-				_ = RunBeyla(t.Context(), &cfg)
+				_ = Run(t.Context(), &cfg)
 			})
 		})
 	}
