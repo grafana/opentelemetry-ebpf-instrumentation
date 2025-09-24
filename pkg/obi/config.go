628978f6713e5769f86732f1671ad4d1f7dd9fac
Nikola Grcevski <grcevski@gmail.com>
2025-09-24 08:40:49 -0400

    fix tests and change to 15 seconds

--- pkg/obi/config.go
+++ pkg/obi/config.go
@@ -106,7 +106,7 @@ var DefaultConfig = Config{
 		Protocol:          otelcfg.ProtocolUnset,
 		TracesProtocol:    otelcfg.ProtocolUnset,
 		MaxQueueSize:      4096,
-		BatchTimeout:      60 * time.Second,
+		BatchTimeout:      15 * time.Second,
 		ReportersCacheLen: ReporterLRUSize,
 		Instrumentations: []string{
 			instrumentations.InstrumentationALL,
