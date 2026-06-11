1b07c15ea15526c43fe62b865a1bba562d10ab55
Artavazd Balaian <reasyu@gmail.com>
2026-06-11 08:23:22 +0800

    Add JVM heap runtime metrics

--- pkg/export/feature.go
+++ pkg/export/feature.go
@@ -35,6 +35,7 @@ const (
 	FeatureGraph
 	FeatureApplicationHost
 	FeatureApplicationRuntime
+	FeatureApplicationJVM
 	FeatureEBPF
 	FeatureAll = Features(^uint(0)) // all bits to 1
 )
@@ -62,6 +63,7 @@ var FeatureMapper = map[string]Features{
 	"application_service_graph":    FeatureGraph,
 	"application_host":             FeatureApplicationHost,
 	"application_runtime":          FeatureApplicationRuntime,
+	"application_jvm":              FeatureApplicationJVM,
 	"ebpf":                         FeatureEBPF,
 	"all":                          FeatureAll,
 	"*":                            FeatureAll,
@@ -142,7 +144,7 @@ func (f Features) Empty() bool {
 }
 
 func (f Features) AnyAppO11yMetric() bool {
-	return f.any(AppO11yFeatures | FeatureApplicationRuntime)
+	return f.any(AppO11yFeatures | FeatureApplicationRuntime | FeatureApplicationJVM)
 }
 
 func (f Features) SpanMetrics() bool {
@@ -161,6 +163,8 @@ func (f Features) AppOrSpan() bool {
 	return f.any(FeatureApplicationRED |
 		FeatureSpanSizes |
 		FeatureApplicationHost |
+		FeatureApplicationRuntime |
+		FeatureApplicationJVM |
 		FeatureSpanLegacy |
 		FeatureSpanOTel)
 }
@@ -179,6 +183,10 @@ func (f Features) AppHost() bool {
 
 func (f Features) AppRuntime() bool {
 	return f.any(FeatureApplicationRuntime)
+}
+
+func (f Features) AppJVM() bool {
+	return f.any(FeatureApplicationJVM)
 }
 
 func (f Features) AppRED() bool {
